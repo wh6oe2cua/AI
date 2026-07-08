9388ad062fd954be5db164841991e2230eb19243
wh6oe2cua <hw26a008@oecu.jp>
2026-07-08 15:06:04 +0900

    build: update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/noir/' : '/',
+    base: process.env.GITHUB_ACTIONS ? '/wh6oe2cua.github.io/noir//' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
